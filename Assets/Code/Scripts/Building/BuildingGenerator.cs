9cbd76eee7e9ead8ff5d7eee189a806d35467ff5
BRARD Clément <brard.clement.83@gmail.com>
2026-01-21 09:32:53 +0100

    continu exterior & start building generator

---
+++ Assets/Code/Scripts/Building/BuildingGenerator.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class BuildingGenerator : MonoBehaviour
+{
+    public GameObject[] basePrefabs;
+    public GameObject floorPrefab;
+    public GameObject[] roofPrefabs;
+}
