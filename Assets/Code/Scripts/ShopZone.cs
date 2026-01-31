2d7b0b209ea00a26b440f21a6e2238a3f48ee51b
BRARD Clément <brard.clement.83@gmail.com>
2026-01-31 22:49:12 +0100

    get out old element in shopzone

--- Assets/Code/Scripts/ShopZone.cs
+++ Assets/Code/Scripts/ShopZone.cs
@@ -9,10 +9,6 @@ public class ShopZone : MonoBehaviour
     [SerializeField] private string nameShop;
     [SerializeField] private TypeShop shopType;
     [SerializeField] private Image icnShop;
-    [SerializeField] private float textYOffset = 0.5f; 
-    [SerializeField] private Color color = Color.yellow;
-    private Material mat;
-    private SphereCollider sphere;
 
     private int levelShop = 1;
     private int xpAct = 0;
@@ -26,7 +22,6 @@ public class ShopZone : MonoBehaviour
     public void SetNameShop(string nameMod)
     {
         nameShop = nameMod;
-        //nameObj.text = nameShop.ToUpper();
     }
 
     public void SetTypeShop(int typeValue)
@@ -34,16 +29,8 @@ public class ShopZone : MonoBehaviour
         shopType = (TypeShop)typeValue;
     }
 
-
-     void Awake()
-    {
-        sphere = GetComponent<SphereCollider>();
-        mat = new Material(Shader.Find("Hidden/Internal-Colored"));
-    }
-
     void Update()
     {
-
         /*if(playerIn && Keyboard.current.pKey.wasPressedThisFrame)
         {
             AddXp(60);
